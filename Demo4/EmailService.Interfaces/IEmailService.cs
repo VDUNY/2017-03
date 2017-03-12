ace78c6c4de2b7e468b0c2b4a46cfa323b3bfed3
S. Kyle Korndoerfer <kyle.korndoerfer@gmail.com>
2017-03-12 10:55:20 -0400

    Added the Email service interface project

---
+++ Demo4/EmailService.Interfaces/IEmailService.cs
@@ -0,0 +1,10 @@
+﻿using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace EmailService.Interfaces
+{
+    public interface IEmailService
+    {
+		Task<bool> SendEmail( MailAddressCollection toAddresses, MailAddress fromAddress, string subject, string body );
+    }
+}
