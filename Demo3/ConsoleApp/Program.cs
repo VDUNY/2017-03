084eaecc2bcd0f5784e65099084fae056ea45e73
S. Kyle Korndoerfer <kyle.korndoerfer@gmail.com>
2017-05-03 22:26:54 -0400

    Upgrades for the latest Service Fabric SDK & a slight rework of Demo3 (looping logic)

--- Demo3/ConsoleApp/Program.cs
+++ Demo3/ConsoleApp/Program.cs
@@ -15,18 +15,22 @@ namespace ConsoleApp
 			var cancellationToken = new CancellationToken();
 			var actorId = ActorId.CreateRandom();
 
+			Console.WriteLine( "Current Counter value: 0\n" );
+
 			var input = -1;
 			while ( input != 0 )
 			{
-				// why create a new instance every time we loop?  :)
-				var actor = ActorProxy.Create<IDemoActor>( actorId, "fabric:/Demo3" );
-
-				Console.WriteLine( "Current Counter value: {0}\n", actor.GetCountAsync( cancellationToken ).GetAwaiter().GetResult() );
-
 				Console.Write( "Enter a new counter value (0 to exit): " );
 				input = Int32.Parse( Console.ReadLine() );
 
-				actor.SetCountAsync( input, cancellationToken ).GetAwaiter().GetResult();
+				if ( input != 0 )
+				{
+					// why create a new instance every time we loop?  :)
+					var actor = ActorProxy.Create<IDemoActor>( actorId, "fabric:/Demo3" );
+					actor.SetCountAsync( input, cancellationToken ).GetAwaiter().GetResult();
+				}
+
+				Console.WriteLine( "Current Counter value: {0}\n", actor.GetCountAsync( cancellationToken ).GetAwaiter().GetResult() );
 			}
 
 			Console.ReadKey();
